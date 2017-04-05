04cc0e75f03a4a1309175fb716ad435f2e325e97
Daniel Brierton <daniel@brierton.me>
2017-04-05 15:07:02 +0100

    Moved crowdin-cli installation to build.sh. Removed branch name check

--- build.sh
+++ build.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
-if [ "$CIRCLE_BRANCH" = "master" ]; then
-	# Configure git for pushing
-	git config --global user.email "webteam@coderdojo.com"
-	git config --global user.name "CoderDojo Foundation"
+# Install crowdin-cli and deps
+gem install mime-types -v 2.6.2
+gem install rest-client -v 1.8
+gem install crowdin-cli
 
-	# Configure NPM
-	echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
-	
-	# Crowdin
-	crowdin-cli upload sources
-	for lang in it pt-PT de nl ro mt sl sk fr uk es-ES es-AR el pl et bg tr ja hi lt; do
-		crowdin-cli download -l $lang
-	done
+# Configure git for pushing
+git config --global user.email "webteam@coderdojo.com"
+git config --global user.name "CoderDojo Foundation"
 
-	# Bump version, push and publish
-	npm version patch
-	git push
-	npm publish
-fi
+# Configure NPM
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+
+# Crowdin
+crowdin-cli upload sources
+for lang in it pt-PT de nl ro mt sl sk fr uk es-ES es-AR el pl et bg tr ja hi lt; do
+	crowdin-cli download -l $lang
+done
+
+# Bump version, push and publish
+npm version patch
+git push
+npm publish
