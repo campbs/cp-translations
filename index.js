f84129a0e42f4bbc078336821fdb886c2b7219c4
Daniel Brierton <daniel@brierton.me>
2017-04-05 14:08:44 +0100

    Added some additional helper methods

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+var path = require('path');
+
 module.exports = {
+  getPoFilePath: function(poFile) {
+  	return path.join(__dirname, 'po-files', poFile)
+  },
+
+  getEmailTemplatePath: function (emailTemplateCode) {
+  	return path.join(__dirname, 'email-templates', emailTemplateCode);
+  },
+
   getFilePath: function (path) {
-    return __dirname + path;
+    return path.join(__dirname, path);
   }
 };
