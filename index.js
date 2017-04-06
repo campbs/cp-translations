ca8e3698d7e95691e7e8af0316265139f4181fc0
Daniel Brierton <daniel@brierton.me>
2017-04-06 16:21:22 +0100

    Fixed index.js, updated messages.po

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var path = require('path');
 
 module.exports = {
-  getPoFilePath: function(poFile) {
-  	return path.join(__dirname, 'po-files', poFile)
+  getPoFilePath: function(poFile, locale) {
+  	return path.join(__dirname, 'po-files', locale, poFile)
   },
 
   getEmailTemplatePath: function (emailTemplateCode) {
-  	return path.join(__dirname, 'email-templates', emailTemplateCode);
+  	return path.join(__dirname, 'email-templates', emailTemplateCode || '');
   },
 
-  getFilePath: function (path) {
-    return path.join(__dirname, path);
+  getFilePath: function (filePath) {
+    return path.join(__dirname, filePath);
   }
 };
