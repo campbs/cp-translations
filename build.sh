61202af62ed6d96e81d24c35ae9219af6d8b884a
Daniel Brierton <daniel@brierton.me>
2017-04-19 14:23:58 +0100

    Revert to Crowdin CLI 2.x and NPM 2.x

--- build.sh
+++ build.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
-# Install crowdin-cli and deps
-gem install mime-types -v 2.6.2
-gem install rest-client -v 1.8
-gem install crowdin-cli -v 0.5.5
+# Install crowdin-cli
+curl -o ~/crowdin.deb https://artifacts.crowdin.com/repo/deb/crowdin.deb
+sudo dpkg -i ~/crowdin.deb
 
 # Configure git for pushing
 git config --global user.email "webteam@coderdojo.com"
 git config --global user.name "CoderDojo Foundation"
 
 # Configure NPM
-echo "_auth=$NPM_TOKEN" >> ~/.npmrc
-echo "email=webteam@coderdojo.com" >> ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 
 # Crowdin
-crowdin-cli upload sources
+crowdin upload sources
 for lang in it pt-PT de nl ro mt sl sk fr uk es-ES es-AR el pl et bg tr ja hi lt; do
-	crowdin-cli download -l $lang
+	crowdin download -l $lang
 done
 
 # Bump version, push and publish
